03dc5292073f1b023e206f34c3e3f96f9a6fea4e
Thanh-Tuan-VKU <pttuan38@gmail.com>
2026-04-08 22:03:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        string connStr = "Server=.\\SQLEXPRESS01;Database=TestADO;Trusted_Connection=True;TrustServerCertificate=True";
+
+        SqlConnection conn = new SqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+            Console.WriteLine("Ket noi thanh cong!");
+
+            string sql = "SELECT * FROM SinhVien";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("ID: " + reader["id"]);
+                Console.WriteLine("Name: " + reader["name"]);
+                Console.WriteLine("Age: " + reader["age"]);
+                Console.WriteLine("-------------------");
+            }
+
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Loi: " + ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
