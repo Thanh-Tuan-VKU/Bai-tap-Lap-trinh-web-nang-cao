11c2ce9bb8d411f7b6032fa2cd124b55db86c8e1
Thanh-Tuan-VKU <pttuan38@gmail.com>
2026-04-08 22:10:35 +0700

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+        Console.WriteLine("Tao instance");
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void ShowMessage()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+
+        s1.ShowMessage();
+        s2.ShowMessage();
+
+        if (s1 == s2)
+        {
+            Console.WriteLine("Cung instance!");
+        }
+    }
+}
